bbe071efb57e94ba98b13cdd33e48a95d210c6f5
Wagner Cavalcante Santos <wasantos@bionexo.com>
2020-05-06 12:06:30 -0300

    CRUD

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props =>
+  <Main icon="home" title="inicio"
+    subtitle="Segundo Projeto do capitulo de React">
+    <div className="display-4">
+      Bem vindo!
+    </div>
+    <hr />
+    <p className="mb-0">
+      Sistema para exemplificar a construção de um cadastrado desenvolvido em React!
+    </p>
+  </Main>
